52a7bdc5e67deeaf6c773336e18916bff25ed28e
Felipe G <pipe2705@gmail.com>
2020-01-20 18:30:24 -0800

    Adds the ability to POST a Job to the database and confirms GET request.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let database = require("./database.js");
 // Create our express app
 let app = express();
 
+app.use(express.json());
+
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
@@ -30,6 +32,36 @@ app.get("/api/Jobs", (req, res) => {
       res.sendStatus(500);
     } else {
       res.status(200).json(results);
+    }
+  });
+});
+
+//Create One JOB
+
+app.post("/api/Jobs", (req, res) => {
+  let createNewJob = [
+    req.body.jobId,
+    req.body.position,
+    req.body.company,
+    req.body.salary,
+    req.body.city_location
+  ];
+  let insertNewJob = "INSERT INTO Jobs VALUES ( ?, ?, ?, ?, ?)";
+  database.run(insertNewJob, createNewJob, error => {
+    if (error) {
+      console.log("Could not add a Job to the Jobs Table", error);
+      res.sendStatus(500);
+    } else {
+      let getAllJobs = "SELECT * FROM Jobs";
+
+      database.all(getAllJobs, (error, results) => {
+        if (error) {
+          console.log("Get all Jobs table failed", error);
+          res.sendStatus(500);
+        } else {
+          res.status(200).json(results);
+        }
+      });
     }
   });
 });
--- seed.js
+++ seed.js
@@ -2,54 +2,39 @@ const db = require("./database.js");
 
 const jobsList = [
   {
+    jobId: 1,
     position: "Product Manager",
     company: "General Assembly",
     salary: 20000,
-    city_location: "San Francisco",
-    country_location: "USA",
-    profession_id: 1,
-    week_posted: 30,
-    is_fulltime: 1
+    city_location: "San Francisco"
   },
   {
+    jobId: 1,
     position: "Product Manager",
-    company: "Assembly General",
-    salary: 25000,
-    city_location: "San Francisco",
-    country_location: "USA",
-    profession_id: 1,
-    week_posted: 30,
-    is_fulltime: 0
+    company: "General Assembly",
+    salary: 20000,
+    city_location: "San Francisco"
   },
   {
+    jobId: 1,
     position: "Product Manager",
-    company: "TASK Rabbit",
-    salary: 130000,
-    city_location: "San Francisco",
-    country_location: "USA",
-    profession_id: 1,
-    week_posted: 47,
-    is_fulltime: 1
+    company: "General Assembly",
+    salary: 20000,
+    city_location: "San Francisco"
   },
   {
-    position: "Associate Product Manager",
-    company: "SISENSE",
-    salary: null,
-    city_location: "San Francisco",
-    country_location: "USA",
-    profession_id: 4,
-    week_posted: 46,
-    is_fulltime: 1
-  },
-  {
+    jobId: 1,
     position: "Product Manager",
-    company: "Accellion",
-    salary: 123000,
-    city_location: "Palo Alto",
-    country_location: "USA",
-    profession_id: 1,
-    week_posted: 43,
-    is_fulltime: 1
+    company: "General Assembly",
+    salary: 20000,
+    city_location: "San Francisco"
+  },
+  {
+    jobId: 1,
+    position: "Product Manager",
+    company: "General Assembly",
+    salary: 20000,
+    city_location: "San Francisco"
   }
 ];
 
